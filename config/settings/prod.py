9ebc0453af40eb1c3a597899878ef4baec4df45d
내MoonHyunHee <judge030@naver.com>
2023-02-19 15:47:26 +0900

    settings 파일 수정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,14 @@
-from .base import *  # base에 있는 모든 내용을 사용한다. ALLOWED_HOSTS만 따로 사용한다.
+'''
+파일명: prod.py
+설명:
+생성일 : 2023-02-19
+생성자: judge
+since 2023.01.09 Copyright (c) by KandJang All right reserved.
+'''
+from .base import BASE_DIR
+from .prod import *
 import environ
-ALLOWED_HOSTS = ['43.200.51.48'] #AWS 고정  IP
+ALLOWED_HOSTS = ['43.200.51.48'] #AWS 고정 IP
 STATIC_ROOT = BASE_DIR / 'static/'
 #이유는 STATIC_ROOT가 설정된 경우 STATICFILES_DIRS 리스트에 STATIC_ROOT와 동일한 디렉터리가 포함되어 있으면
 # 서버 실행 시 다음과 같은 오류가 발생
